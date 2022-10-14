6f1f6530d47f5b12bfcb8324ddd725e7049fc221
Zhiff11 <ghifariaprilia@gmail.com>
2022-10-15 00:49:16 +0700

    Cuman test

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+echo("=========
+Kalkulator
+==========
+
+Nilai 1 = 8
+Nilai 2 = 10
+==========");
+
+$a = (8);
+$b = (10);
+
+echo 'Perkalian '.($a * $b);
+echo 'Jumlah '.($a + $b);
+echo 'Kurang '.($a - $b);
+echo 'Bagi '.($a / $b)
+?>
